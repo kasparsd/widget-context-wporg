d1ae9ed5f0d7b67cb146b87f112c98ab159e9c26
Kaspars <kaspars@konstruktors.com>
2013-03-16 14:03:56 +0100

    hide widget controls after saving

--- widget-context.js
+++ widget-context.js
@@ -2,6 +2,10 @@ jQuery(window).ready(function($) {
 
 	$('.widget-context-inside, .toggle-contexts .collapse').hide();
 
+	$(document).ajaxComplete( function( e, xhr, settings ) {
+		$('.widget-context-inside, .toggle-contexts .collapse').hide();
+	});
+
 	$('.widget-context .toggle-contexts').live( 'click', function() {
 		$(this).siblings('.widget-context-inside').slideToggle('fast');
 		$('span', this).toggle();
