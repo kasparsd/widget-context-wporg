dd12fc289de83df703e4c4a1858267c33cf1bcb1
Kaspars <kaspars@konstruktors.com>
2013-03-17 18:13:44 +0100

    Correct the context toggle button in backend

--- widget-context.js
+++ widget-context.js
@@ -3,7 +3,12 @@ jQuery(window).ready(function($) {
 	$('.widget-context-inside, .toggle-contexts .collapse').hide();
 
 	$(document).ajaxComplete( function( e, xhr, settings ) {
-		$('.widget-context-inside, .toggle-contexts .collapse').hide();
+		$('.widget-context').each(function() {
+			if ( $( '.widget-context-inside', this ).is(':visible') )
+				$( '.toggle-contexts .expand', this ).hide();
+			else
+				$( '.toggle-contexts .collapse', this ).hide();
+		});
 	});
 
 	$('.widget-context .toggle-contexts').live( 'click', function() {
