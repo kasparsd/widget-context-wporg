b145a5f6102a46862ce4fa9a85ca3d0b92bb9381
Kaspars Dambis <hi@kaspars.net>
2018-06-13 14:30:07 +0300

    Include a few mocks

---
+++ tests/php/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Include the autoloader and register a few mocks.
+ *
+ * @todo Sort out the mocks situation. We don't want to load the whole WP.
+ */
+
+include( __DIR__ . '/../../vendor/autoload.php' );
+
+function wp_parse_args( $args, $defaults ) {
+	return array_merge( $defaults, $args );
+}
+
+function wp_parse_url( $url, $components = -1 ) {
+	return parse_url( $url, $components );
+}
