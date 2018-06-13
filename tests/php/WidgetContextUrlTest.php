cd7fdb2b5a818d3d6f0540e54f91dda52634a828
Kaspars Dambis <hi@kaspars.net>
2018-06-13 13:49:32 +0300

    More tests

--- tests/php/WidgetContextUrlTest.php
+++ tests/php/WidgetContextUrlTest.php
@@ -10,14 +10,16 @@ class WidgetContextTest extends TestCase {
 		$this->plugin = new widget_context();
 	}
 
-	public function testUrlMatchesWithoutQuery() {
-		//var_dump($this->plugin);
-		$this->assertTrue(true);
+	public function testUrlWildcards() {
+		$this->assertTrue( $this->plugin->match_path( 'page/subpage', 'page/*' ) );
+		$this->assertFalse( $this->plugin->match_path( 'another-page', 'page/*' ) );
 	}
 
-	public function testUrlMatchesWithQuery() {
-		//var_dump($this->plugin);
-		$this->assertTrue(true);
+	public function testUrlQueryStrings() {
+		$this->assertTrue( $this->plugin->match_path( 'page/subpage?query=string', 'page/*' ) );
+		$this->assertFalse( $this->plugin->match_path( 'campaigns/?cc=automotive', 'campaigns/' ) );
+		$this->assertTrue( $this->plugin->match_path( 'campaigns/?cc=automotive', 'campaigns/*' ) );
+		$this->assertTrue( $this->plugin->match_path( 'campaigns/?cc=automotive', 'campaigns/?cc=*' ) );
 	}
 
 }
