c79bf35a85fa80863dc3c75a304e5f47a8b1e26d
Kaspars Dambis <hi@kaspars.net>
2018-06-13 15:02:22 +0300

    This is already handled by PHPUnit

--- tests/php/bootstrap.php
+++ tests/php/bootstrap.php
@@ -1,11 +1,10 @@
 <?php
 /**
- * Include the autoloader and register a few mocks.
+ * Include mocks if necessary. Note that phpunit includes the Composer
+ * autoloader automatically.
  *
  * @todo Sort out the mocks situation. We don't want to load the whole WP.
  */
-
-require_once( __DIR__ . '/../../vendor/autoload.php' );
 
 if ( ! function_exists( 'wp_parse_args' ) ) {
 	require __DIR__ . '/wp-mocks.php';
