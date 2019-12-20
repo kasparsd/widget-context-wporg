16e4053571b76756cdd1e74eadb252fd8db63a5b
Kaspars Dambis <hi@kaspars.net>
2019-12-20 13:56:53 +0200

    Test the request path resolver

--- tests/php/WidgetContextTest.php
+++ tests/php/WidgetContextTest.php
@@ -2,11 +2,27 @@
 
 namespace Preseto\WidgetContextTest;
 
+use WP_Mock;
 use Mockery;
 use WidgetContext;
 use Preseto\WidgetContextTest\WidgetContextTestCase;
 
 class WidgetContextTest extends WidgetContextTestCase {
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->plugin = new \WidgetContext( null );
+
+		WP_Mock::userFunction( 'wp_parse_args' )
+			->andReturnUsing(
+				function( $args, $defaults ) {
+					return array_merge( $defaults, $args );
+				}
+			);
+
+		WP_Mock::alias( 'wp_parse_url', 'parse_url' );
+	}
 
 	public function testLegacyInstance() {
 		$plugin = Mockery::mock( 'Preseto\WidgetContext\Plugin' );
@@ -21,6 +37,18 @@ class WidgetContextTest extends WidgetContextTestCase {
 		$this->assertInstanceOf(
 			get_class( $widget_context ),
 			WidgetContext::instance()
+		);
+	}
+
+	public function testRequestPathResolver() {
+		$this->assertEquals(
+			'path-to-a/url.html?true=2',
+			$this->plugin->get_request_path( 'https://example.com:8999/path-to-a/url.html?true=2' )
+		);
+
+		$this->assertEquals(
+			'path-to-a/url.html',
+			$this->plugin->get_request_path( 'path-to-a/url.html' )
 		);
 	}
 
