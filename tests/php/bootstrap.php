a97c665648e78911845210a214640c72a375bde7
Kaspars Dambis <hi@kaspars.net>
2021-10-31 12:45:31 +0200

    Patch the notice

--- tests/php/bootstrap.php
+++ tests/php/bootstrap.php
@@ -4,3 +4,12 @@
  */
 
 WP_Mock::bootstrap();
+
+/**
+ * Patch the following error in PHP 8:
+ * Uncaught Error: Class "PHP_Token_NAME_QUALIFIED" not found
+ * in vendor/phpunit/php-token-stream/src/Token/Stream.php:189
+ */
+if ( ! class_exists( 'PHP_Token_NAME_QUALIFIED' ) ) {
+	class PHP_Token_NAME_QUALIFIED extends PHP_Token {}
+}
